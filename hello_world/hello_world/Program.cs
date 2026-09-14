cef12e5700ebdbe6ca0060089877b9526c793101
PROKY01 <L20240054@prumyslovkajihlava.cz>
2026-09-14 22:40:54 +0200

    Automaticka zaloha

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT3A_EPO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cesta k souboru
+            string cestaKSouboru = "c:\\Users\\jirpr\\skola\\PVA\\pokus.txt";
+
+            //test existence souboru
+            if (File.Exists(cestaKSouboru))
+            {
+                //soubour existuje
+                Console.WriteLine("Soubor nalezen");
+
+                //do souboru pripojime proud (ctecka text. souboru)
+                using (StreamReader sr = File.OpenText(cestaKSouboru))
+                {
+                    //deklaruji si promennou pro jeden znak
+                    int cislo;
+                    //jdeme cist znaku po znaku
+                    while ((cislo = sr.Read()) != -1)
+                    {
+                        //cislo prevedeme na znak
+                        string znak = Char.ConvertFromUtf32(cislo);
+                        //vytiskni znak
+                        Console.Write(znak);
+                    }
+                    //uzavreme cteci proud
+                    sr.Close();
+                }
+            }
+            else
+            {
+                //soubor na zadane ceste neexistuje
+                Console.WriteLine("Soubor nenalezen");
+            }
+
+            //cekame na stisk klavesy
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hello_world/hello_world/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("hello_world")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hello_world")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("91735184-0dfb-4791-b8ba-cde14d7e36d7")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
